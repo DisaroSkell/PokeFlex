9c64703834df63d0a1ebf2bcee9947cba7bdf9d5
DisaroSkell <Vous@exemple.com>
2022-02-24 14:13:23 +0100

    Changer de pokemon sur details marche

--- component/PokemonDetail.js
+++ component/PokemonDetail.js
@@ -67,6 +67,56 @@ app.component('pokemon-detail', {
             })
         })
     },
+    watch: {
+        id() {
+            this.url = "https://pokeapi.co/api/v2/pokemon/" + this.id.toString() + "/"
+            P.resource([
+                this.url
+            ]).then( resource => {
+                this.name = capitilize(resource[0].name)
+                this.sprite = resource[0].sprites.other["official-artwork"].front_default
+                this.height = resource[0].height / 10.
+                this.weight = resource[0].weight / 10.
+                this.abilities = resource[0].abilities
+
+                for(let i = 0; i<this.abilities.length; i++) {
+                    this.abilities[i].ability.name = capitilize(this.abilities[i].ability.name)
+                }
+
+                this.stats = resource[0].stats
+                P.resource([
+                    resource[0].species.url
+                ]).then( resource2 => {
+                    this.location = capitilize(resource2[0].habitat.name)
+                    let baddesc = resource2[0].flavor_text_entries[0].flavor_text.split('\n')
+                    this.desc = ""
+
+                    for(let i = 0; i<baddesc.length; i++) {
+                        if (i != 0) {
+                            this.desc += " "
+                        }
+                        this.desc += baddesc[i]
+                    }
+
+                    baddesc = this.desc.split('\f')
+                    this.desc = ""
+
+                    for(let i = 0; i<baddesc.length; i++) {
+                        if (i != 0) {
+                            this.desc += " "
+                        }
+                        this.desc += baddesc[i]
+                    }
+
+                    for(let i = 0; i<resource2[0].genera.length; i++) {
+                        if(resource2[0].genera[i].language.name == "en") {
+                            this.specie = resource2[0].genera[i].genus
+                        }
+                    }
+                })
+            })
+        }
+    },
     computed: {
         idS() {
             return (1000 + this.id).toString().slice(-3)
